ca31048f31441216838f5bb1e36c30171dcce049
kbritt510 <kbritt510@gmail.com>
2020-09-18 22:09:20 -0500

    Finished project

--- app/controllers/owned_stocks_controller.rb
+++ app/controllers/owned_stocks_controller.rb
@@ -1,30 +1,15 @@
 class OwnedStocksController < ApplicationController
 
-get '/owned_stocks' do 
-    @owned_stocks = current_user.owned_stocks
-    erb :owned_stock_index
+  get '/owned_stocks' do 
+      @owned_stocks = current_user.owned_stocks
+      erb :owned_stock_index
+    end
+
+  post '/owned_stocks/new' do
+      @new_owned_stock = Stock.create(params)
+      owned_stock = OwnedStock.create(user_id:current_user.id, stock_id:@new_owned_stock.id)
+      erb :'/owned_stocks/new_owned_stock'
   end
-
-post '/owned_stocks/new' do
-    @new_owned_stock = Stock.create(params)
-    owned_stock = OwnedStock.create(user_id:current_user.id, stock_id:@new_owned_stock.id)
-    erb :'/owned_stocks/new_owned_stock'
-end
-
-# get '/owned_stocks/show' do
-#   @new_owned_stock = Stock.last
-#   erb :'owned_stocks/show'
-# end
-
-# get '/owned_stocks/new' do
-#     new_stock = Stock.create(params)
-#     @owned_stock = OwnedStock.create(user_id:current_user.id, stock_id:params[:id])
-#     erb :new_owned_stock
-# end
-
-  
-
-
 
   get '/owned_stocks/:id/edit' do
     if logged_in?
@@ -73,7 +58,11 @@ end
   get '/owned_stocks/:id' do
     if logged_in?
       @owned_stock = OwnedStock.find_by(id: params[:id])
-      erb :'owned_stocks/show'
+      if @owned_stock.user_id == current_user.id
+        erb :'owned_stocks/show'
+      else
+        redirect to '/failure'
+      end
     else
       redirect to '/failure'
     end
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -10,14 +10,14 @@ class StocksController < ApplicationController
     end
   end
 
-  # GET: /stocks/new
   get "/stocks/new" do
     erb :"/stocks/new.html"
   end
 
-  # POST: /stocks
-  post "/stocks" do
-    redirect "/stocks"
+  post '/stocks/:id/add_new_stock' do
+    @stock = Stock.find(params[:id])
+    new_stock = OwnedStock.find_or_create_by(user_id:current_user.id, stock_id:@stock.id)
+    redirect to '/users/show'
   end
 
   get '/stocks/:id' do
@@ -29,18 +29,4 @@ class StocksController < ApplicationController
     end
   end
 
-  # GET: /stocks/5/edit
-  get "/stocks/:id/edit" do
-    erb :"/stocks/edit.html"
-  end
-
-  # PATCH: /stocks/5
-  patch "/stocks/:id" do
-    redirect "/stocks/:id"
-  end
-
-  # DELETE: /stocks/5/delete
-  delete "/stocks/:id/delete" do
-    redirect "/stocks"
-  end
 end
